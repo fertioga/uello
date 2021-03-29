4655b39c4030975b85fc9a85cd99218b9009dfb6
Fernando França <fertioga@gmail.com>
2021-03-29 14:17:55 -0300

    ajuste

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,8 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
+
   NODE_ENV: '"development"',   
   VUE_APP_MAPBOX_KEY: '"pk.eyJ1IjoiZmVydGlvZ2EiLCJhIjoiY2ttb3hhNzlkMjh6YjJ1bWkxb3kxYWx4dyJ9.BfwyZFFMx1T034PrJwHuAg"'
+
 })
